332af502bb7e600f2916a44b36cd7fc91fb9b08d
Sharath Koppu <sharathk91@gmail.com>
2018-06-22 09:00:18 -0500

    added comments

--- ICP6/NB.py
+++ ICP6/NB.py
@@ -1,22 +1,31 @@
+#import packages for datasets,metrics,Gaussian NB model and test for training data
 from sklearn import datasets,metrics
 from sklearn import svm
 from sklearn.naive_bayes import GaussianNB
 #from sklearn.cross_validation import train_test_split
 from sklearn.model_selection import train_test_split
 
-
+#load the iris datasets
 irisdatasets=datasets.load_iris()
+#load the irisdatasets column values
 x=irisdatasets.data
 y=irisdatasets.target
 
 #split the data for training and testing cross validation
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
-
+#define the model
 model=GaussianNB()
+#fit the training data into model
 model.fit(x_train,y_train)
+#prints the probability of training data
 print(model.score(x_train,y_train))
+#define to predict the test data
 y_pred = model.predict(x_test)
+#calc accuracy
 print("Accuracy",metrics.accuracy_score(y_test, y_pred))
+#calc precision
 print("Precision:",metrics.precision_score(y_test,y_pred,average='weighted'))
+#calc Recall
 print("Recall:",metrics.recall_score(y_test,y_pred,average='weighted'))
+#calc F1 score
 print("F1 Score:",metrics.f1_score(y_test,y_pred,average='weighted'))
--- ICP6/kmeans.py
+++ ICP6/kmeans.py
@@ -1,31 +1,49 @@
+#import pandas library
 import pandas
 import pylab as pl
+#import kmeans
 from sklearn.cluster import KMeans
+#import pca for calc elbow curve
 from sklearn.decomposition import PCA
+#import the csv file
 variables = pandas.read_csv('sample_stocks.csv')
+#load the column values of dataset
 Y = variables[['returns']]
 X = variables[['dividendyield']]
+#calc normal values
 X_norm = (X - X.mean()) / (X.max() - X.min())
 Y_norm = (Y - Y.mean()) / (Y.max() - Y.min())
+#define the range
 Nc = range(1, 20)
+#define the range to calc kmeans algorithm for range of 20 nos
 kmeans = [KMeans(n_clusters=i) for i in Nc]
 kmeans
+#dalc score for all kmeans ranging from 1 to 20 nos
 score = [kmeans[i].fit(Y).score(Y) for i in range(len(kmeans))]
 score
+#plot the elbow curve
 pl.plot(Nc,score)
+#define the labels
 pl.xlabel('Number of Clusters')
 pl.ylabel('Score')
 pl.title('Elbow Curve')
+#show the elbow curve
 pl.show()
+#define the components to fit into model
 pca = PCA(n_components=1).fit(Y)
+#calc cluster values
 pca_d = pca.transform(Y)
 pca_c = pca.transform(X)
+#call the kmeans with k=3
 kmeans=KMeans(n_clusters=3)
+#define the kmeans to fit into model
 kmeansoutput=kmeans.fit(Y)
 kmeansoutput
+#define the labels
 pl.figure('3 Cluster K-Means')
 pl.scatter(pca_c[:, 0], pca_d[:, 0], c=kmeansoutput.labels_)
 pl.xlabel('Dividend Yield')
 pl.ylabel('Returns')
 pl.title('3 Cluster K-Means')
+#sho the plotted graph for 3 clusters
 pl.show()
